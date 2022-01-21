b4d971ed739ab2e3cd19a1b6b23367b9ca4e5b5d
Eduardo Moreira <Du_du.mba@hotmail.com>
2022-01-20 21:25:47 -0300

    first commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,6 +8,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final textController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +20,32 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Container(
         height: double.infinity,
-        
+        padding: EdgeInsets.all(10),
+        alignment: Alignment.center,
+        child: Column(
+          children: [
+            TextField(
+              controller: textController,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'cep'),
+            ),
+            SizedBox(height: 10),
+            Container(
+              height: 50,
+              width: 200,
+              child: ElevatedButton(
+                  onPressed: () {
+                    // searchCep(textController.text);
+                  },
+                  child: Text('Pesquisar')),
+            ),
+            SizedBox(height: 20),
+               SizedBox(height: 20),
+              //  if (isLoading) Expanded(child: Center(child: CircularProgressIndicator())),
+              //   if (error != null) Text(error, style: TextStyle(color: Colors.black),),
+              //   if (!isLoading && cepResult.isNotEmpty) Text("Cidade: ${cepResult['localidade']}"),
+          ],
+        ),
       ),
     );
   }
